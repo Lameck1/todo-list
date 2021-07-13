ff1160825c4d278c9794aeb346a954f67094380d
Lameck1 <lameck72@gmail.com>
2021-07-13 12:26:22 +0300

    Add TaskModel class
    The task model class is just for serving the controller with data. Currently it only has hard coded list of tasks to work with.

---
+++ src/assets/js/models/taskModel.js
@@ -0,0 +1,8 @@
+export default class TaskModel {
+  static tasks = [
+    { id: 1, description: 'Take child to school', complete: false },
+    { id: 2, description: 'Learn about webpack', complete: true },
+    { id: 3, description: 'Pick child from school', complete: false },
+    { id: 4, description: 'Go shopping for groceries', complete: true },
+  ]
+}
