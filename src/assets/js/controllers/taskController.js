2f417391f24b76526ba74e48ebe54dd1753cc4f3
Lameck1 <lameck72@gmail.com>
2021-07-15 02:32:53 +0300

    Add #handleToggleTaskStatus
    This methods update completeness status of a give task

--- src/assets/js/controllers/taskController.js
+++ src/assets/js/controllers/taskController.js
@@ -1,11 +1,22 @@
 import TaskView from '../views/taskView';
 import TaskModel from '../models/taskModel';
 import dragHandler from '../helpers/dragHandler';
+import getElementAll from '../helpers/getElementAll';
 
 export default () => ({
 
   taskModel: new TaskModel(),
   taskView: new TaskView(),
+
+  handletoggleTaskStatus() {
+    getElementAll('.checkbox').forEach((checkbox) => {
+      checkbox.addEventListener('change', () => {
+        TaskModel.toggleTaskStatus(Number(checkbox.getAttribute('data-id')));
+        localStorage.setItem('tasks', JSON.stringify(TaskModel.tasks));
+        window.location.reload();
+      });
+    });
+  },
 
   init() {
     if (localStorage.getItem('tasks') === null) {
@@ -13,5 +24,6 @@ export default () => ({
     }
     this.taskView.showTasks(TaskModel.tasks);
     dragHandler(this.taskView, TaskModel.tasks);
+    this.handletoggleTaskStatus();
   },
 });
