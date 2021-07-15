e515975f34fceaed440873f82436aaa3121556fe
Lameck1 <lameck72@gmail.com>
2021-07-15 21:22:20 +0300

    Bugfix #dragHandler

--- src/assets/js/helpers/dragHandler.js
+++ src/assets/js/helpers/dragHandler.js
@@ -1,7 +1,8 @@
 import getElementAll from './getElementAll';
 import getElement from './getElement';
+import refactorIndex from './refactorIndex';
 
-export default (view, tasks) => {
+export default (tasks) => {
   const taskItems = getElementAll('.task-container');
   const taskList = getElement('.task-list');
   let draggedId = 0;
@@ -24,7 +25,7 @@ export default (view, tasks) => {
     item.addEventListener('dragstart', (event) => {
       event.target.classList.add('dragging');
       draggedId = Number(event.target.getAttribute('data-id'));
-      draggedItem = tasks.splice(draggedId, 1);
+      draggedItem = tasks.splice(draggedId - 1, 1);
     });
 
     item.addEventListener('dragend', (event) => {
@@ -49,15 +50,18 @@ export default (view, tasks) => {
     const afterNode = dragAfterNode(taskList, event.clientY);
     if (!afterNode) {
       tasks = [...tasks, ...draggedItem];
+      refactorIndex(tasks);
       localStorage.setItem('tasks', JSON.stringify(tasks));
     }
 
     if (afterNode) {
-      if (draggedId > Number(afterNode.getAttribute('data-id'))) {
-        tasks.splice(Number(afterNode.getAttribute('data-id')), 0, draggedItem[0]);
+      if (draggedId > Number(afterNode.getAttribute('data-id')) - 1) {
+        tasks.splice(Number(afterNode.getAttribute('data-id')) - 1, 0, draggedItem[0]);
+        refactorIndex(tasks);
         localStorage.setItem('tasks', JSON.stringify(tasks));
       } else {
-        tasks.splice(Number(afterNode.getAttribute('data-id')) - 1, 0, draggedItem[0]);
+        tasks.splice(Number(afterNode.getAttribute('data-id')) - 2, 0, draggedItem[0]);
+        refactorIndex(tasks);
         localStorage.setItem('tasks', JSON.stringify(tasks));
       }
     }
