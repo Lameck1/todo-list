9b469695a54dfd8fdf0c2fd4104e1251e3a8c5eb
Lameck1 <lameck72@gmail.com>
2021-07-15 11:10:45 +0300

    Remove hard coded tasks objects

--- src/assets/js/models/taskModel.js
+++ src/assets/js/models/taskModel.js
@@ -1,15 +1,8 @@
 export default class TaskModel {
-  static tasks = JSON.parse(localStorage.getItem('tasks'));
-
-  static samples = [
-    { id: 1, description: 'Take child to school', complete: false },
-    { id: 2, description: 'Learn about webpack', complete: true },
-    { id: 3, description: 'Pick child from school', complete: false },
-    { id: 4, description: 'Go shopping for groceries', complete: true },
-  ];
+  static tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
   static refreshStorage() {
-    localStorage.setItem('tasks', JSON.stringify(TaskModel.samples));
+    localStorage.setItem('tasks', JSON.stringify(TaskModel.tasks));
   }
 
   static toggleTaskStatus(index) {
